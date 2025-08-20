63f3974da185f74359a39e0aa90c5ebfaee65aa8
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-08-20 08:30:09 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def get():
+    return {"Hello": "world"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
